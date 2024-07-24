af92f86dad10558c7e9e79949debc0a91b07647b
jlopezgomez <joselpzgomez@gmail.com>
2024-07-24 12:35:28 +0200

    subida

---
+++ src/com/pildorasinformaticas/Git.java
@@ -0,0 +1,10 @@
+package com.pildorasinformaticas;
+
+public class Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hola amigos del mundo");
+	}
+
+}
